3bec630e2fd7235bcd1f3162cac24c5b13cdcf35
Chris Green <cgreenws@gmail.com>
2019-03-29 11:51:02 -0500

    Got copy working again after reseting.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BaseFormComponent } from './base-form/base-form.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: BaseFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
